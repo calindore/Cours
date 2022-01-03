78a08fa93018494178e7f05ce8e6b349bd198885
Gauthier Antoine <calindore@gmail.com>
2022-01-03 10:47:04 +0100

    Fonctionnel
    -Comparaison des valeurs
    -Nombre d'essai

--- devinenombre/js/main.js
+++ devinenombre/js/main.js
@@ -1,6 +1,7 @@
 console.log("main.js - Chargé");
 
 let target = getRandomInt(0,100) //Numéro entre 0 et 100 exclu
+let essais = 0;
 console.log(target);
 
 function getRandomInt(min, max) {
@@ -8,7 +9,29 @@ function getRandomInt(min, max) {
 
 }
 
-function showInputValue() {
-      let input = document.querySelector("#deviner");
-      console.log(input.value);
+// function showInputValue() {
+//       let input = document.querySelector("#deviner");
+//       console.log(input.value);
+// }
+
+function comparewithtarget() {
+      let guess = document.querySelector("#deviner").value;
+
+      let status = document.querySelector("#annonce");
+
+      if (guess > target) {
+            status.innerHTML = "Plus Petit";
+      } else if (guess < target) {
+            status.innerHTML = "Plus Grand";
+      } else {
+            status.innerHTML = "C'est le bon !"
+      }
+}
+
+function ajoutessai() {
+      essais++;
+      
+      let htmlessai = document.querySelector("#essais");
+
+      htmlessai.innerHTML = essais + " Essai";
 }
